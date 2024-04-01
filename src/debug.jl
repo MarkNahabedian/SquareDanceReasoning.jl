8143259866efae851359beb672b62b140afec181
MarkNahabedian <naha@mit.edu>
2024-04-01 10:31:22 -0400

    Move some more XML related code out of debug.jl.

--- src/debug.jl
+++ src/debug.jl
@@ -97,20 +97,6 @@ function dancer_formations_html(kb::ReteRootNode)
 end
 
 
-xml_id_letter(::Guy) = "m"
-xml_id_letter(::Gal) = "f"
-xml_id_letter(::Unspecified) = "u"
-
-xml_id(d::Dancer) = "dancer$(d.couple_number)$(xml_id_letter(d.gender))"
-
-
-gender_css_symbol(::Guy) = "href" => "#Guy"
-gender_css_symbol(::Gal) = "href" => "#Gal"
-gender_css_symbol(::Unspecified) = "href" => "#Neutral"
-
-const DANCER_SVG_SIZE = 20
-
-
 function bounds_to_viewbox(bounds::Bounds)
     bounds = bump_out(bounds, 2)
     width = abs(DANCER_SVG_SIZE * (bounds.max_left - bounds.min_left))
--- src/xml/dancer_symbols.jl
+++ src/xml/dancer_symbols.jl
@@ -14,3 +14,18 @@ function __init__()
     end
 end
 
+
+xml_id_letter(::Guy) = "m"
+xml_id_letter(::Gal) = "f"
+xml_id_letter(::Unspecified) = "u"
+
+xml_id(d::Dancer) = "dancer$(d.couple_number)$(xml_id_letter(d.gender))"
+
+
+gender_css_symbol(::Guy) = "href" => "#Guy"
+gender_css_symbol(::Gal) = "href" => "#Gal"
+gender_css_symbol(::Unspecified) = "href" => "#Neutral"
+
+const DANCER_SVG_SIZE = 20
+
+
