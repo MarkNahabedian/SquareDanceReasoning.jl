12b70b097566586ae3789ca0ffa6aa1007fde054
MarkNahabedian <naha@mit.edu>
2024-04-02 14:35:56 -0400

    animate

---
+++ src/xml/animate.jl
@@ -0,0 +1,51 @@
+export animate
+
+function animate(timeline::Dict{Dancer, Vector{DancerState}})
+    bounds = Bounds()
+    for dss in values(timeline)
+        bounds = expand(bounds, dss)
+    end
+    bounds = bump_out(bounds)
+    duration = let
+        t(ds) = ds.time
+        dss = Iterators.flatten(values(timeline))
+        maximum(t, dss) - minimum(t, dss)
+    end
+    duration *= 0.5
+    x(ds) = DANCER_SVG_SIZE * ds.left
+    y(ds) = DANCER_SVG_SIZE * ds.down
+    rot(ds) = @sprintf("rotate(%3.0f %3.2f %3.2f)",
+                       90 - 360 * ds.direction,
+                       x(ds), y(ds))
+    elt("svg",
+        "xmlns" => SVG_NAMESPACE,
+        bounds_to_viewbox(bounds)...,
+        DANCER_SYMBOLS...,
+        elt("g") do a
+            for (dancer, dss) in timeline
+                sorted = sort(dss; by = ds -> ds.time)
+                a(elt("use",
+                      "width" => DANCER_SVG_SIZE,
+                      "height" => DANCER_SVG_SIZE,
+                      "class" => daner_color(dancer),
+                      gender_css_symbol(dancer.gender),
+                      elt("animate",
+                          "dur" => duration,
+                          "repeatCount" => "indefinite" ,
+                          "attributeName" => "x",
+                          "values" => join(map(x, sorted), "; ")),
+                      elt("animate",
+                          "dur" => duration,
+                          "repeatCount" => "indefinite" ,
+                          "attributeName" => "y",
+                          "values" => join(map(y, sorted), "; ")),
+                      elt("animate",
+                          "dur" => duration,
+                          "repeatCount" => "indefinite" ,
+                          "attributeName" => "transform",
+                          "values" => join(map(rot, sorted), "; "))))
+            end
+        end
+        )
+end
+
--- src/xml/load.jl
+++ src/xml/load.jl
@@ -5,7 +5,24 @@ using Printf
 
 const SVG_NAMESPACE = "http://www.w3.org/2000/svg"
 
+function bounds_to_viewbox(bounds::Bounds)
+    bounds = bump_out(bounds, 2)
+    width = abs(DANCER_SVG_SIZE * (bounds.max_left - bounds.min_left))
+    height = abs(DANCER_SVG_SIZE * (bounds.max_down - bounds.min_down))
+    [
+        "viewBox" =>
+            @sprintf("%3.3f %3.3f %3.3f %3.3f",
+                     DANCER_SVG_SIZE * bounds.min_left,
+                     DANCER_SVG_SIZE * bounds.min_down,
+                     width, height),
+        "width" => width,
+        "height" => height
+    ]
+end
+
+
 include("elt.jl")
 include("dancer_symbols.jl")
 include("dancer_colors.jl")
+include("animate.jl")
 
