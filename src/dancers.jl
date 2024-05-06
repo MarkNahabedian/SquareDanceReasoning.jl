a53b2b79bc648a1d8b4b9877504520b888ac150f
MarkNahabedian <naha@mit.edu>
2024-05-06 13:35:09 -0400

    Should have been in previous change.

--- src/dancers.jl
+++ src/dancers.jl
@@ -4,7 +4,6 @@ export Gender, Guy, Gal, Unspecified, opposite
 export Dancer, OriginalPartners
 export is_original_head, is_original_side
 export OriginalPartnerRule, OriginalPartners, couple_number
-export SDSQuare, SquareHasDancers, make_square
 
 
 """
@@ -129,61 +128,3 @@ returns true if the dancer was originally in a side position.
 """
 is_original_side(d::Dancer)::Bool = iseven(d.couple_number)
 
-
-"""
-    SDSquare(dancers)
-
-SDSquare represents the dancers in a square.
-
-`make_square(number_of_dancers)` will return an `SDSquare`.
-
-SDSquare is a fact that can be asserted to the knowledge base to
-inform it that the dancers form a square.
-
-`other_dancers(::SDSquare, dancers)` returns the set of `Dancer`s
-that are in the SDSquare but are not in `dancers`.
-"""
-struct SDSquare
-    dancers::Set{Dancer}
-
-    SDSquare(dancers) = new(Set{Dancer}(dancers))
-end
-
-
-"""
-    other_dancers(square::SDSquare, dancers)
-
-Returns the dancers from `square` that are not listed in the `dancers`
-argument.
-"""
-other_dancers(square::SDSquare, dancers) =
-    setdiff(square.dancers, dancers)
-
-
-@rule SquareDanceRule.SquareHasDancers(square::SDSquare, ::Dancer) begin
-    for dancer in square.dancers
-        emit(dancer)
-    end
-end
-
-@doc """
-SquareHasDancers is a convenience rule for asserting the
-`Dancer`s from a `SDSquare`.
-""" SquareHasDancers
-
-
-
-"""
-    make_square(number_of_couples::Int)::SDSquare
-
-Returns an SDSquare with the specified number of couples.
-"""
-function make_square(number_of_couples::Int)::SDSquare
-    dancers = Vector{Dancer}()
-    for couple_number in 1:number_of_couples
-        for gender in [Guy(), Gal()]
-            push!(dancers, Dancer(couple_number, gender))
-        end
-    end
-    SDSquare(dancers)
-end
