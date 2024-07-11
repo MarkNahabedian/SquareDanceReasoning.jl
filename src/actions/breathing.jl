ccd77697ef8e9c9630205858708e9bd7674b9cfc
MarkNahabedian <naha@mit.edu>
2024-07-11 07:01:30 -0400

    New breathing algorithm.

--- src/actions/breathing.jl
+++ src/actions/breathing.jl
@@ -1,126 +1,98 @@
 using LinearAlgebra: dot, normalize!
 
-export MoveApart, move_apart, breathe
-
-
-"""
-    MoveApart(center, displacement)
-    MoveApart(collision::Collision, playmates)
-
-struct MoveApart can be applied to a DancerState to determine
-how much it should be displaced to resolve `collision`.
-
-`center` and `displacement` are two element Vectors.
-
-`playmates` is a Vector of Vectors of `DancerState`s where the
-elements of each inner vector are working together.
-"""
-struct MoveApart
-    center::Vector
-    displacement::Vector
-    # The dancer that moves by displacement rather than by
-    # -displacement:
-    positive_displacement_ds::DancerState
-
-    function MoveApart(center::Vector,
-                       displacement::Vector,
-                       positive_displacement_ds::DancerState)
-        @assert length(center) == 2
-        @assert length(displacement) == 2
-        new(center, displacement, positive_displacement_ds)
-    end
-end
-
-function MoveApart(collision::Collision, playmates)
-    # playmates is a Vector of Vector of DancerState:
-    @assert all(playmates) do pms
-        all(pms) do pm
-            pm isa DancerState
-        end
-    end
-    # Find the playmate for each collident
-    other_a, other_b = let
-        playmates_a = playmates[findfirst(playmates) do pm
-                                    collision.a in pm
-                                end]
-        @assert length(playmates_a) == 2
-        playmates_b = playmates[findfirst(playmates) do pm
-                                    collision.b in pm
-                                end]
-        @assert length(playmates_b) == 2
-        (first(setdiff(Set(playmates_a), Set([collision.a]))),
-         first(setdiff(Set(playmates_b), Set([collision.b]))))
-    end
-    displacement_amount = COUPLE_DISTANCE / 2
-    if distance(collision.a, other_a) > distance(collision.a, other_b)
-        # colliders overshot their center:
-        displacement_amount += distance(collision.a, collision.b) / 2
-    end
-    # Direction to move the collision.a dancer:
-    uv = normalize!(location(other_a) - location(collision.a))
-    MoveApart(center(dancer_states(collision)),
-              displacement_amount * uv,
-              collision.a)
-end
-
+export breathe
 
 """
-    (mp::MoveApart)(ds::DancerState)
-
-For the given DancerState, determine how much `mp` should displace it.
-"""
-function (mp::MoveApart)(ds::DancerState)
-    loc = location(ds)
-    # On what side of the center is the DancerState?
-    side = sign(dot(loc - mp.center, mp.displacement))
-    # It might be dead-on center.
-    if side == 0
-        if ds == mp.positive_displacement_ds
-            side = 1
-        else
-            side = -1
-        end
-    end
-    side * mp.displacement
-end
-
-
-"""
-    move_apart(mps::Vector{MoveApart}, ds::DancerState)::DancerState
-
-Applies all of the MoveApart displacements to the specified
-DancerState.
-"""
-function move_apart(mps::Vector{MoveApart},
-                    ds::DancerState)::DancerState
-    new_loc = location(ds)
-    for mp in mps
-        new_loc += mp(ds)
-    end
-    # We splice te DancerSTate that we move_apart from out of the
-    # dancer history since its location was just a temporary
-    # calculation aid.
-    DancerState(ds.previous, ds.time, ds.direction, new_loc...)
-end
-
-
-"""
-    breathe(collisions::Vector{Collision}, playmates::Vector{Vector{DancerState}}, everyone::Vector)::Vector{DancerStates}
+    breathe(collisions::Vector{Collision}, playmates::Vector{TwoDancerFormation}, everyone::Vector)::Vector{DancerState}
 
 Moves the dancers apart such that those in `collisions` no longer
-overlap.  Those grouped together in `playmates` are moved together,
-and remain fixed relative to one another.  `everyone` includes all
-DancerStates that are to be affected.
+overlap.  `playmates` is used to inform what direction colliders
+should be moved in.
+
+`everyone` includes all DancerStates that are to be affected.
 """
-function breathe(collisions::Vector,
-                 playmates::Vector,
-                 everyone::Vector
+function breathe(collisions::Vector{Collision},
+                 playmates::Vector{<:TwoDancerFormation},
+                 everyone::Vector{DancerState}
                  )::Vector{DancerState}
-    mps = map(collisions) do c
-        MoveApart(c, playmates)
+    d2ds = Dict{Dancer, DancerState}()
+    flagpole = center(map(location, everyone))
+    dancers = []
+    accumulated_movement = Dict{Dancer, Vector}()
+    for ds in everyone
+        push!(dancers, ds.dancer)
+        d2ds[ds.dancer] = ds
+        accumulated_movement[ds.dancer] = [0, 0]
     end
-    map(everyone) do ds
-        move_apart(mps, ds)
+    current_location(d::Dancer) =
+        location(d2ds[d]) + accumulated_movement[d]
+    function next_collision()
+        n = length(dancers)
+        for i in 1:(n-1)
+            for j in (i+1):n
+                a = current_location(dancers[i])
+                b = current_location(dancers[j])
+                if distance(a, b) < DANCER_COLLISION_DISTANCE
+                    return [dancers[i], dancers[j]]
+                end
+            end
+        end
+        nothing
     end
+    limit = 20
+    while (begin
+               collision = next_collision()
+               collision != nothing
+           end)
+        if limit <= 0
+            error("limit exceeded")
+        end
+        limit -= 1
+        (d1, d2) = collision
+        # We arrange that d1 is the dancer whose playmate is further
+        # from flagpole.  We use their playmates because d1 and d2
+        # might have moved past each other when colliding.
+        if playmate(d2, playmates) == nothing
+            error("no playmate for $d2")
+        end
+        if playmate(d1, playmates) == nothing
+            error("no playmate for $d1")
+        end
+        if (distance(current_location(playmate(d2, playmates)), flagpole) >
+            distance(current_location(playmate(d1, playmates)), flagpole))
+            (d1, d2) = (d2, d1)
+        end
+        # What direction do we move d1?  Towards its playmate:
+        move_d1 = normalize!(current_location(playmate(d1, playmates))
+                             - current_location(d1)) * COUPLE_DISTANCE
+        move_d1 += dot(current_location(d2) - current_location(d1),
+                       normalize(move_d1)) * normalize(move_d1)
+        c = center([current_location(d1),
+                    current_location(d2)])
+        for d in dancers
+            # Never move d2 -- it might be at the same location as d1:
+            if d == d2
+                continue
+            end
+            # Only move the dancers who are in the move_d1 direction
+            # from c:
+            if dot(current_location(d) - c, move_d1) < 0
+                continue
+            end
+            # Make sure that the magnitude is increasing:
+            mag2(v) = sum(x -> x^2, v)
+            m1 = mag2(accumulated_movement[d])
+            accumulated_movement[d] += move_d1
+            m2 = mag2(accumulated_movement[d])
+            if m2 < m1
+                error("movement decreased for $d in breathing")
+            end
+        end
+    end
+    result = map(everyone) do ds
+        DancerState(ds, ds.time, ds.direction,
+                    current_location(ds.dancer)...)
+    end
+    result
 end
 
