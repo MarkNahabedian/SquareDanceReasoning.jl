7cf4d8ca15f07ddb2b585c0209217170bda4dbc1
MarkNahabedian <naha@mit.edu>
2024-07-15 19:21:13 -0400

    get_call_options.  Fixes some problems.  Exposes others.

--- src/calls/calls.jl
+++ src/calls/calls.jl
@@ -105,26 +105,59 @@ function do_call(kb::ReteRootNode, call::SquareDanceCall)::ReteRootNode
     return kb
 end
 
-function do_simple_call(kb::ReteRootNode,
-                        call::SquareDanceCall)::Vector{DancerState}
-    receive(kb, call)
-    # Where can we do that call from?
+function get_call_options(call::SquareDanceCall,
+                          kb::ReteRootNode)::Vector{CanDoCall}
+    # What calls can we do from where?
     options = askc(Collector{CanDoCall}(), kb, CanDoCall)
     options = filter!(options) do cdc
         cdc.call == call
     end
     # Restrict by role:
     options = filter!(options) do cdc
+        # Every dancer in the formation satisfies the role
+        # restriction:
         length(dancer_states(those_with_role(cdc.formation, call.role))) ==
             length(dancer_states(cdc.formation))
     end
-    # Find highest preference options:
-    options = sort!(options;
-                    rev = true,
-                    by = cdc -> cdc.preference)
-    options = filter(options) do cdc
-        cdc.preference == options[1].preference
+    # Find highest preference option for each dancer:
+    preferred = Dict{DancerState, CanDoCall}()
+    for opt in options
+        for ds in dancer_states(opt.formation)
+            if haskey(preferred, ds)
+                if opt.preference == preferred[ds].preference
+                    error("$opt and $(preferred[ds]) conflict.")
+                elseif opt.preference > preferred[ds].preference
+                    preferred[ds] = opt
+                end
+            else
+                preferred[ds] = opt
+            end
+        end
     end
+    # Consolidate the options, making sure that there are no two
+    # options that concern the same dancer:
+    do_these = Vector{CanDoCall}()
+    for (ds, opt) in preferred
+        if in(opt, do_these)
+            for dto in do_these
+                if dto == opt
+                    continue
+                end
+                if ds in dancer_states(dto.formation)
+                    error("$(ds.dancer) in both $opt and $dto")
+                end
+            end
+        else
+            push!(do_these, opt)
+        end
+    end
+    do_these
+end
+
+function do_simple_call(kb::ReteRootNode,
+                        call::SquareDanceCall)::Vector{DancerState}
+    receive(kb, call)
+    options = get_call_options(call, kb)
     # Should we make sure that the formations in the remaining options
     # are disjoint?
     everyone = askc(Collector{DancerState}(), kb, DancerState)
