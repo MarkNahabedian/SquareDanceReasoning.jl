ad15a1ee8c306f534b3a805b2c2fb88ddb5e69ac
MarkNahabedian <naha@mit.edu>
2024-07-19 07:57:44 -0400

    Add roles to the Balance test.  We can see from the resulting SVG files that the Guys and Gals balance separately.  We are gratified.

--- test/test_calls/test_one_dancer_calls.jl
+++ test/test_calls/test_one_dancer_calls.jl
@@ -13,8 +13,8 @@
     original_dss = sort!(askc(Collector{DancerState}(), kb, DancerState);
                          by = ds -> ds.dancer)
     @debug_formations(kb)
-    kb = do_call(kb, Balance())
-    kb = do_call(kb, Balance())
+    kb = do_call(kb, Balance(role=Guys()))
+    kb = do_call(kb, Balance(role=Gals()))
     dss = sort!(askc(Collector{DancerState}(), kb, DancerState);
                 by = ds -> ds.dancer)    
     for i in 1:length(original_dss)
