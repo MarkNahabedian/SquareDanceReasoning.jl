392a82a42aa92fa97403565496b467292bce4c3e
MarkNahabedian <naha@mit.edu>
2024-07-30 14:40:54 -0400

    Doc string for SquareDanceCall

--- src/calls/call_engine.jl
+++ src/calls/call_engine.jl
@@ -3,6 +3,21 @@ export SquareDanceCall, can_do_from, do_call, expand_parts,
 
 # Should SquareDanceCall <: TemporalFact
 # so that they don't persist across successive knowledge bases?
+
+"""
+    SquareDanceCall
+
+For each call, a struct is defined that is a subtype of
+`SquareDanceCall`.
+
+For each call and each formation it can be done from, a
+[`can_do_from`](@ref) method should be defined.  If the "Ocean Wave
+Rule" applies, then a `can_do_from` method should be defined for that
+case.
+
+For each call and each formation it can be performed from, either a
+`perform` or an `expand_parts` method should be defined.
+"""
 abstract type SquareDanceCall end
 
 
