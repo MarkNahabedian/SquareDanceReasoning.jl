b136bf18736862dd2f07b47674ccf7ea0a7ca87d
MarkNahabedian <naha@mit.edu>
2024-09-07 17:33:21 -0400

    showcase SquareThru_from_SquaredSet.

--- docs/showcase.jl
+++ docs/showcase.jl
@@ -24,6 +24,25 @@ let
              )
 end
 
+let
+    square = make_square(4)
+    kb = make_kb()
+    receive(kb, square)
+    receive.([kb], square_up(square))
+    write_formation_html_file("Squared Set",
+                              abspath("src/formation_drawings/squared_set.html"),
+                              kb)
+    showcase("SquareThru_from_SquaredSet",
+             "Heads Square Thru from a squared set",
+             kb,
+             SquareDanceCall[
+                 SquareThru(role = OriginalHead()),
+                 _Rest(time = 2)
+             ];
+             inhibit_call_engine_logging = false
+             )
+end
+
 
 # This sould be the last expression in this file.
 make_showcase_index_file()
