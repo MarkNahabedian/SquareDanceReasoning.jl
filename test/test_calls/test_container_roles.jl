96cda805a20fa600e005b9f90736a2867fb60d08
MarkNahabedian <naha@mit.edu>
2025-01-29 18:54:46 -0500

    More test assertioons, inspired by animation and formation debug SVG results.

--- test/test_calls/test_container_roles.jl
+++ test/test_calls/test_container_roles.jl
@@ -14,22 +14,38 @@
         @debug_formations(kb)
         containment_graph(kb, "test_centers_trade", DotBackend(@__DIR__, "test_centers_trade"))
         # Check that those_with_role is working:
-        let
-            line = only(askc(Collector{LineOfFour}(), kb))
-            @test Set(those_with_role(line, kb, Centers())) ==
-                Set(dancer_states(line.centers))
-            @test Set(those_with_role(line.centers, kb, Centers())) ==   # this fails
-                Set(dancer_states(line.centers))
-        end
-        let
-            wave = only(askc(Collector{WaveOfFour}(), kb))
-            @test Set(those_with_role(wave, kb, Centers())) ==
-                Set(dancer_states(wave.centers))            
-            @test Set(those_with_role(wave.centers, kb, Centers())) ==
-                Set(dancer_states(wave.centers))
-        end
+        line = only(askc(Collector{LineOfFour}(), kb))
+        @test Set(those_with_role(line, kb, Centers())) ==
+            Set(dancer_states(line.centers))
+        @test Set(those_with_role(line.centers, kb, Centers())) ==   # this fails
+            Set(dancer_states(line.centers))
+        wave = only(askc(Collector{WaveOfFour}(), kb))
+        @test Set(those_with_role(wave, kb, Centers())) ==
+            Set(dancer_states(wave.centers))            
+        @test Set(those_with_role(wave.centers, kb, Centers())) ==
+            Set(dancer_states(wave.centers))
         kb = do_call(kb, Trade(role=Centers()))
         @debug_formations(kb)
+        # BUG: The animation shows that there are both timing and breathing issues.
+        # the centers in the line pause in the middle of their trade.
+        # The centers of the wave are no longer lined up with the rest of the wave.
+        pos(ds::DancerState) = (ds.down, ds.left)
+        let
+            line2 = only(askc(Collector{InvertedLineOfFour}(), kb))
+            @test line2.centers.beau.direction == opposite(line.centers.belle.direction)
+            @test line2.centers.belle.direction == opposite(line.centers.beau.direction)
+            @test pos(line2.centers.beau) == pos(line.centers.belle)
+            @test pos(line2.centers.belle) == pos(line.centers.beau)
+        end
+        #= TEST_FAILS: Doesn't work because, due to breathing issues, we don't see a wave
+        let
+            wave2 = only(askc(Collector{WaveOfFour}(), kb))
+            @test wave2.centers.a.direction == wave.centers.b.direction
+            @test wave2.centers.b.direction == line.centers.a.direction
+            @test pos(wave2.centers.a) == pos(wave.centers.b)
+            @test pos(wave2.centers.b) == pos(wave.centers.a)
+        end
+        =#
         animate(joinpath(@__DIR__, "centers_trade.svg"),
                 askc(Collector{DancerState}(), kb, DancerState),
                 40)
