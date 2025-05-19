63226ffa4843424400d55b322666896abc7426b9
MarkNahabedian <naha@mit.edu>
2025-05-19 11:43:47 -0400

    A bunch of changes to make the call engine cope with zero diration calls like _EndAt.
    Some of this is kind of a hack.  We might want to restructure
    do_schedule in the near furure.
    
    Added specific exceptions UnsynchronizedDancersException and
    DancerStatesNotNewestException for some call engine related errors.
    
    Add more @info logging.

--- src/calls/call_engine.jl
+++ src/calls/call_engine.jl
@@ -1,6 +1,40 @@
 export can_do_from, do_call, expand_parts, perform
 
 
+abstract type CallEngineException <: Exception end
+
+"""
+    UnsynchronizedDancersException
+
+An exception which can be signaled by the call engine if the relevant
+DancerStates are not symchronized to one another.
+"""
+struct UnsynchronizedDancersException <: CallEngineException
+    dancer_states::Vector{DancerState}
+end
+
+function Base.showerror(io::IO, e::UnsynchronizedDancersException)
+    print(io, "DancerStates do not have the same time: $(e.dancer_states)")
+end
+
+
+"""
+    DancerStatesNotNewestException
+
+An exception which can be signaled by the call engine if the
+DancerStates being considered for a call are not the newest
+DancerStates for their Dancers.
+"""
+struct DancerStatesNotNewestException <: CallEngineException
+    formation::SquareDanceFormation
+    not_newest::Vector{Tuple{Int, DancerState}}
+end
+
+function Base.showerror(io::IO, e::DancerStatesNotNewestException)
+    print(io, "Some dancers in $(e.formation) are not newest: $(e.not_newest).")
+end
+
+
 """
     restricted_to(call)::Role
 
@@ -65,16 +99,11 @@ function do_call(kb::SDRKnowledgeBase, call::SquareDanceCall;
                  dbgctx = nothing)::SDRKnowledgeBase
     @info("do_call", call)
     sched = let
-        dss = askc(Collector{DancerState}(), kb, DancerState)
+        dss = askc(Collector{DancerState}(), kb)
         if allequal(map(ds -> ds.time, dss))
             CallSchedule(dss[1].time)
         else
-            for dancer in sort(dss; by = ds -> ds.dancer)
-                history(newest_dancer_states[dancer]) do ds
-                    println("\t\t$(ds.time):\t$(ds.direction)\t$(ds.down)\t$(ds.left)")
-                end
-            end
-            error("DancerStates do not have the same time: $dss")
+            throw(UnsynchronizedDancersException(dss))
         end
     end
     schedule(sched, call, sched.now)
@@ -90,7 +119,7 @@ function do_schedule(sched::CallSchedule, kb::SDRKnowledgeBase;
             # >= rather than > because some actions like breathing or
             # the _EndAt call create new DancrStates without advancing
             # its time.
-            if ds.time >= newest_dancer_states[ds.dancer].time
+            if history_position(ds) >= history_position(newest_dancer_states[ds.dancer])
                 newest_dancer_states[ds.dancer] = ds
             end
         else
@@ -141,6 +170,19 @@ function do_schedule(sched::CallSchedule, kb::SDRKnowledgeBase;
             end
             write_debug_formation_file(dbgctx, kb, sched.now)
             while (!isempty(sched)) && sched.now == peek(sched).when
+                # Temporary experimental hack while debugging the call
+                # engine behavior regarding zero duration calls like
+                # _EndAt.  We wouldn't need this is we adopted ordinal
+                # based scheduling.
+                if (!isempty(call_history) &&
+                    call_schedule_isless(last(call_history)[2].scheduled_call.call,
+                                         peek(sched).call))
+                    kb = make_kb(kb)
+                    for ds in values(newest_dancer_states)
+                        receive(kb, ds)
+                    end
+                    @info("do_schedule updated knowledgebase for zero diration call hack", kb)
+                end
                 # Process a queue entry, performing it if it's "atomic" or
                 # requeueing its parts.
                 #
@@ -153,7 +195,7 @@ function do_schedule(sched::CallSchedule, kb::SDRKnowledgeBase;
                 @assert now_do_this isa ScheduledCall
                 let
                     done = 0
-                    # get_call_options only considers formations that match sched.now.
+                    # get_call_options only considers formations that match now_do_this.when
                     options = get_call_options(now_do_this, kb)
                     @info("do_schedule get_call_options returned", options)
                     for cdc in options
@@ -161,7 +203,12 @@ function do_schedule(sched::CallSchedule, kb::SDRKnowledgeBase;
                             not_newest = filter(ds -> ds != newest_dancer_states[ds.dancer],
                                                 collect(cdc.formation()))
                             if !isempty(not_newest)
-                                error("Some dancers in $(cdc.formation) are not newest: $not_newest")
+                                throw(DancerStatesNotNewestException(
+                                    cdc.formation,
+                                    [
+                                        (history_position(ds), ds)
+                                        for ds in not_newest
+                                            ]))
                             end
                         end
                         e = expand_cdc(cdc)
@@ -182,7 +229,7 @@ function do_schedule(sched::CallSchedule, kb::SDRKnowledgeBase;
                     end
                     @assert done > 0
                 end
-            end
+            end      # while (!isempty(sched)) && sched.now == peek(sched).when
             @info("do_schedule schedule", queue = sched.scheduled_calls)
             # We have finished all of the calls that were scheduled for a
             # given time.
@@ -236,9 +283,11 @@ function do_schedule(sched::CallSchedule, kb::SDRKnowledgeBase;
                 for ds in values(newest_dancer_states)
                     receive(kb, ds)
                 end
+                @info("do_schedule updated knowledgebase", kb)
             end
-        end
-        # Synchronize the dancers for furure calls:
+        end    # while !isempty(sched)
+        # Synchronize the dancers who lag behind sched.now for furure
+        # calls:
         for ds in values(newest_dancer_states)
             if ds.time < sched.now
                 update_newest(DancerState(ds,
@@ -269,16 +318,36 @@ function get_call_options(sc::ScheduledCall, kb::SDRKnowledgeBase)::Vector{CanDo
     # This needs to be a Set to avoid the duplicates we would get from
     # querying for SquareDanceFormation and it subtypes.  Maybe this
     # should be fixed in Rete.Collector.
-    formations = Set{SquareDanceFormation}()
-    everyone = DancerState[]
+    #
+    # We also need to make sure that for a given formation we have the
+    # DancerStates with the greatest history_position.
+    formation_key(f::SquareDanceFormation) =
+        (typeof(f), [ ds.dancer for ds in f() ]...)
+    formations = Dict()
     askc(kb, SquareDanceFormation) do f
-        if f isa DancerState
-            push!(everyone, f)
-        end
-        if timeof(f) == now
-            push!(formations, f)
+        if timeof(f) <= now
+            # The formation must exist at the time of the ScheduledCall.
+            # Testing against now is not sufficient.  We must also
+            # make sure we get the greatest history_position for each
+            # DancerState
+            key = formation_key(f)
+            if haskey(formations, key)
+                if f != formations[key]
+                    # It's not the one we already have:
+                    existing_hist = map(history_position, dancer_states(formations[key]))
+                    fds_hist = map(history_position, dancer_states(f))
+                    @assert length(existing_ds_hist) == length(fds_hist)
+                    if any(map(>=, f_hist, existing_hist))
+                        # f has more history:
+                        formations[key] = f
+                    end
+                end
+            else
+                formations[key] = f
+            end
         end
     end
+    formations = values(formations)
     # What calls can we do from where?
     options = CanDoCall[]
     for f in formations
@@ -287,8 +356,8 @@ function get_call_options(sc::ScheduledCall, kb::SDRKnowledgeBase)::Vector{CanDo
             push!(options, CanDoCall(p, sc, f))
         end
     end
+    @info("get_call_options formations", now, call, formations, kb)
     if length(options) == 0
-        @info("get_call_options formations", now, call, everyone, formations, kb)
         error("No options for $call")
     end
     @info("get_call_options initial options", options)
